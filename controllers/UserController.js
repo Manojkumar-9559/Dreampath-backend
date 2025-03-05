d70d4cdc92d8b844d6b201188dff729d90bd5899
Manojkumar-9559 <manojbasivireddy04@gmail.com>
2025-03-05 19:34:27 +0530

    Switched to bcryptjs

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const db = require('../db');
 const admin = require('../FirebaseAdmin');
 const nodemailer = require('nodemailer');
